610ef9d9aad9e8ac2e44ab6501d6a23b8985126f
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-19 09:52:34 -0400

    feat: connect store to the app with Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import reducers from "./reducers/index";
 import middleware from "./middleware/index";
+import { Provider } from "react-redux";
 // Components
 import App from "./components/App";
 
@@ -12,4 +13,9 @@ import "./index.css";
 // Create the Redux Store:
 const store = createStore(reducers, middleware);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
