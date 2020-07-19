ef41e169a75763cdc9732a2b24fc01feb16554a2
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-19 16:06:21 -0400

    feat: create map over poll ids

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -3,10 +3,24 @@ import { connect } from "react-redux";
 
 export class PollList extends Component {
   render() {
-    return <div>I'm the Poll List</div>;
+    const { pollIds } = this.props;
+    console.log(pollIds);
+    return (
+      <ul>
+        {pollIds.map((id) => (
+          <li key={id}>Hey There!</li>
+        ))}
+      </ul>
+    );
   }
 }
 
-const mapStateToProps = () => {};
+const mapStateToProps = ({ polls, authedUser }) => {
+  return {
+    pollIds: Object.keys(polls).sort(
+      (a, b) => polls[b].timestamp - polls[a].timestamp
+    )
+  };
+};
 
-export default connect()(PollList);
+export default connect(mapStateToProps)(PollList);
