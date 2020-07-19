10e69b72f7cdce7a421156944367b25110270463
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-19 10:35:25 -0400

    feat: add get polls action

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,5 +1,9 @@
+import { GET_POLLS } from "../actions/polls";
+
 export const pollsReducer = (state = {}, action) => {
   switch (action.type) {
+    case GET_POLLS:
+      return { ...state, ...action.polls };
     default:
       return state;
   }
