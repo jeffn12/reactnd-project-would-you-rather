1cb0806be16ba81c444ef45d9bc28caa3810cdfe
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-19 11:02:07 -0400

    feat: add static authed user to store on app load

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,9 @@
 import { _getQuestions, _getUsers } from "../utils/_DATA";
 import { getPolls } from "./polls";
 import { getUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_USER = "tylermcginnis"; //for testing purposes... TODO: authed user will be set from login screen
 
 export const handleInitialData = () => {
   return (dispatch) => {
@@ -8,6 +11,7 @@ export const handleInitialData = () => {
       ([questions, users]) => {
         dispatch(getPolls(questions));
         dispatch(getUsers(users));
+        dispatch(setAuthedUser(AUTHED_USER));
       }
     );
   };
