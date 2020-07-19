b7cdf778be82a0b38685b5ddf65dd3c3583fcdec
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-19 09:39:35 -0400

    feat: create a logger middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+export default logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("Action: ", action.type);
+  const returnValue = next(action);
+  console.log("New State: ", store.getState());
+  console.groupEnd();
+  return returnValue;
+};
