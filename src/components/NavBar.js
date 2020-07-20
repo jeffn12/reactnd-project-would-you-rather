3010da081099c32fc80ab83bbfdb61988b84fb30
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-20 14:38:38 -0400

    feat: update padding on navbar options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,26 +6,27 @@ import { AppBar, Avatar, Box, Toolbar, Typography } from "@material-ui/core";
 export class NavBar extends Component {
   render() {
     const { authedUser } = this.props;
+
     return (
       <AppBar position="static">
         <Toolbar className="nav-bar">
           <Box display="flex" width="100%">
             <Box display="flex" flexDirection="row" flexGrow={1}>
-              <Typography variant="h6" className="nav-link">
+              <Typography variant="h6" style={{ padding: "5px" }}>
                 home
               </Typography>
-              <Typography variant="h6" className="nav-link">
+              <Typography variant="h6" style={{ padding: "5px" }}>
                 leaderboard
               </Typography>
-              <Typography variant="h6" className="nav-link">
+              <Typography variant="h6" style={{ padding: "5px" }}>
                 create a poll
               </Typography>
             </Box>
             <Box display="flex" justifyContent="end" flexShrink={1}>
-              {authedUser && <Avatar src={authedUser.avatarURL} />}
-              <Typography variant="h6" className="nav-link current-user">
-                {authedUser ? `welcome, ${authedUser}!` : "login"}
+              <Typography variant="h6" style={{ padding: "5px" }}>
+                {authedUser ? `welcome, ${authedUser.id}!` : "login"}
               </Typography>
+              {authedUser && <Avatar src={authedUser.avatarURL} />}
             </Box>
           </Box>
         </Toolbar>
