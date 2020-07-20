544223c96d22a2acd46f29f622a919ef3ebea635
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-20 15:19:43 -0400

    feat: control filter with buttons

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,64 @@ import { connect } from "react-redux";
 // Components
 import NavBar from "./NavBar";
 import PollList from "./PollList";
+// Material UI Components
+import { Box, Button, ButtonGroup, Container } from "@material-ui/core";
 // Helpers
 import { handleInitialData } from "../actions/shared";
 import { ANSWERED, NOT_ANSWERED, ALL } from "./PollList";
 
 export class App extends Component {
+  state = {
+    filter: NOT_ANSWERED
+  };
+
   componentDidMount = () => {
     this.props.dispatch(handleInitialData());
   };
 
   render() {
+    const { filter } = this.state;
     return (
-      <div>
+      <Box style={{ justifyContent: "center" }}>
         <NavBar />
-        Would You Rather?
-        <PollList filter={ANSWERED} />
-      </div>
+        <Container>
+          <h2 style={{ textAlign: "center" }}>Choose Which Polls To See</h2>
+          <ButtonGroup size="large" variant="text" fullWidth="true">
+            <Button
+              onClick={() => {
+                this.setState(() => {
+                  return { filter: ANSWERED };
+                });
+              }}
+              color={filter === ANSWERED ? "secondary" : "default"}
+            >
+              answered
+            </Button>
+            <Button
+              onClick={() => {
+                this.setState(() => {
+                  return { filter: ALL };
+                });
+              }}
+              color={filter === ALL ? "secondary" : "default"}
+            >
+              all
+            </Button>
+            <Button
+              onClick={() => {
+                this.setState(() => {
+                  return { filter: NOT_ANSWERED };
+                });
+              }}
+              color={filter === NOT_ANSWERED ? "secondary" : "default"}
+            >
+              not answered
+            </Button>
+          </ButtonGroup>
+          <h3 style={{ textAlign: "center" }}>Would You Rather?</h3>
+          <PollList filter={filter} />
+        </Container>
+      </Box>
     );
   }
 }
