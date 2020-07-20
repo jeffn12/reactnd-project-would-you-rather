d682f302628585041083eec96bcbf0865f74676a
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-20 16:57:29 -0400

    refactor: adjust style (spacing/sizing) of content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,26 +4,41 @@ import { connect } from "react-redux";
 import { AppBar, Avatar, Box, Toolbar, Typography } from "@material-ui/core";
 
 export class NavBar extends Component {
+  getDivider = () => {
+    return (
+      <Typography variant="h6" style={{ padding: "5px" }}>
+        |
+      </Typography>
+    );
+  };
+
   render() {
     const { authedUser } = this.props;
 
     return (
-      <AppBar position="static">
-        <Toolbar className="nav-bar">
+      <AppBar color="primary" position="static">
+        <Toolbar>
           <Box display="flex" width="100%">
             <Box display="flex" flexDirection="row" flexGrow={1}>
               <Typography variant="h6" style={{ padding: "5px" }}>
                 home
               </Typography>
+              {this.getDivider()}
               <Typography variant="h6" style={{ padding: "5px" }}>
                 leaderboard
               </Typography>
+              {this.getDivider()}
               <Typography variant="h6" style={{ padding: "5px" }}>
                 create a poll
               </Typography>
             </Box>
-            <Box display="flex" justifyContent="end" flexShrink={1}>
-              <Typography variant="h6" style={{ padding: "5px" }}>
+            <Box
+              display="flex"
+              justifyContent="end"
+              alignItems="center"
+              flexShrink={1}
+            >
+              <Typography variant="p" style={{ padding: "5px" }}>
                 {authedUser ? `welcome, ${authedUser.id}!` : "login"}
               </Typography>
               {authedUser && <Avatar src={authedUser.avatarURL} />}
