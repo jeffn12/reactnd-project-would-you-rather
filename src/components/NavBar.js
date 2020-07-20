1cd56d22abdb9ed38543294c81d87bfb03e10dc9
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-20 13:46:00 -0400

    feat: update navbar to material-ui (ugh...)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+// Material UI Components
+import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
 
 export class NavBar extends Component {
   render() {
     const { authedUser } = this.props;
     return (
-      <nav className="nav-bar">
-        <a href="/#" className="nav-link">
-          Home
-        </a>
-        <a href="/#" className="nav-link">
-          Leaderboard
-        </a>
-        <a href="/#" className="nav-link">
-          Create a New Poll
-        </a>
-        <a href="/#" className="nav-link current-user">
-          {authedUser}
-        </a>
-      </nav>
+      <AppBar position="static">
+        <Toolbar className="nav-bar">
+          <Box display="flex" width="100%">
+            <Box display="flex" flexDirection="row" flexGrow={1}>
+              <Typography variant="h6" className="nav-link">
+                home
+              </Typography>
+              <Typography variant="h6" className="nav-link">
+                leaderboard
+              </Typography>
+              <Typography variant="h6" className="nav-link">
+                create a poll
+              </Typography>
+            </Box>
+            <Box display="flex" justifyContent="end" flexShrink={1}>
+              <Typography variant="h6" className="nav-link current-user">
+                {authedUser ? `welcome, ${authedUser}!` : "login"}
+              </Typography>
+            </Box>
+          </Box>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
