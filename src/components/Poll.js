1948cf3f072e0c3294e1f21b96751e0af123a41b
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-20 16:18:46 -0400

    feat: use a card to hold each poll in list

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+// Components
+// Material UI Components
+import { Avatar, Card, CardHeader, CardContent } from "@material-ui/core";
+/**
+ * TODO:
+ *  Put each poll on its own 'Card'!
+ */
 
 export class Poll extends Component {
   render() {
-    const { polls, id } = this.props;
+    const { polls, users, id } = this.props;
+    const author = users[polls[id].author];
     return polls[id] ? (
-      <p>{`${polls[id].optionOne.text} or ${polls[id].optionTwo.text}?`}</p>
+      <Card>
+        <CardHeader
+          avatar={<Avatar src={author.avatarURL} />}
+          title={`${author.name} want to know:`}
+          subheader="would you rather..."
+        />
+        {`${polls[id].optionOne.text} or ${polls[id].optionTwo.text}?`}
+      </Card>
     ) : (
-      <p>Not Found</p>
+      <Card>Not Found</Card>
     );
   }
 }
 
-const mapStateToProps = ({ polls }, { id }) => {
+const mapStateToProps = ({ polls, users }, { id }) => {
   return {
     polls,
+    users,
     id
   };
 };
