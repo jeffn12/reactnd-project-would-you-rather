2c0299ed3e4f22ff669db9020ae42c8204f741b4
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-21 12:31:27 -0400

    feat: add action to answer poll

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,8 +1,18 @@
 export const GET_POLLS = "GET_POLLS";
+export const ANSWER_POLL = "ANSWER_POLL";
 
 export const getPolls = (polls) => {
   return {
     type: GET_POLLS,
     polls
+  };
+};
+
+export const answerPoll = (pollId, option, authedUser) => {
+  return {
+    type: ANSWER_POLL,
+    pollId,
+    option,
+    authedUser
   };
 };
--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,9 +1,19 @@
-import { GET_POLLS } from "../actions/polls";
+import { GET_POLLS, ANSWER_POLL } from "../actions/polls";
 
 export const pollsReducer = (state = {}, action) => {
   switch (action.type) {
     case GET_POLLS:
       return { ...state, ...action.polls };
+    case ANSWER_POLL:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state.users[action.authedUser.id],
+          answers: {
+            [action.pollId]: action.option
+          }
+        }
+      };
     default:
       return state;
   }
