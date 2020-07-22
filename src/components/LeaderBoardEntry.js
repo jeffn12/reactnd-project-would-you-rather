37e9c05af29ebe8f8a15594abfc205f71f6f478d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-21 21:27:19 -0400

    feat: add basic user info to entry

--- src/components/LeaderBoardEntry.js
+++ src/components/LeaderBoardEntry.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 // Material UI
-import { Box, Typography } from "@material-ui/core";
+import { Avatar, Box, Typography } from "@material-ui/core";
 
 export const LeaderBoardEntry = (props) => {
   const { user } = props;
+  const { id, name, avatarURL } = user;
 
-  return <Typography>{JSON.stringify(user)}</Typography>;
+  return (
+    <Box my="1rem" display="flex" alignItems="center">
+      <Avatar src={avatarURL} alt={"avatar of " + id} />
+      <Box mx="0.5rem" display="flex" alignItems="center">
+        <Typography variant="body1">{name}</Typography>
+        <Typography variant="subtitle2">({id})</Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LeaderBoardEntry;
