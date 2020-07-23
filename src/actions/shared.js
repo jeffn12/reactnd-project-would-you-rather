b6efccbe1adfbb51a8cf8540185dd2d3d72debc1
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-23 12:46:44 -0400

    feat: create base action for adding new poll

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,17 @@
-import { _getQuestions, _getUsers, _saveQuestionAnswer } from "../utils/_DATA";
+import {
+  _getQuestions,
+  _getUsers,
+  _saveQuestionAnswer,
+  _saveQuestion
+} from "../utils/_DATA";
 import { getPolls } from "./polls";
 import { getUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
 
 export const ANSWER_POLL = "ANSWER_POLL";
+export const ADD_POLL = "ADD_POLL";
 
+// Action Handlers
 export const handleInitialData = () => {
   return (dispatch) => {
     return Promise.all([_getQuestions(), _getUsers()]).then(
@@ -17,7 +24,25 @@ export const handleInitialData = () => {
   };
 };
 
-export const answerPoll = (pollId, option, authedUser) => {
+export const handleAnswerPoll = (pollId, option, authedUser) => {
+  return (dispatch) => {
+    dispatch(answerPoll(pollId, option, authedUser));
+    _saveQuestionAnswer({ authedUser, qid: pollId, answer: option }).catch(
+      (err) => {
+        console.log("there was an error");
+      }
+    );
+  };
+};
+
+export const handleAddPoll = () => {
+  return (dispatch) => {
+    dispatch(addPoll());
+  };
+};
+
+// Action Creators
+const answerPoll = (pollId, option, authedUser) => {
   return {
     type: ANSWER_POLL,
     pollId,
@@ -26,14 +51,8 @@ export const answerPoll = (pollId, option, authedUser) => {
   };
 };
 
-export const handleAnswerPoll = (pollId, option, authedUser) => {
-  return (dispatch) => {
-    dispatch(answerPoll(pollId, option, authedUser));
-    console.log("authed user in handler: ", authedUser);
-    _saveQuestionAnswer({ authedUser, qid: pollId, answer: option }).catch(
-      (err) => {
-        console.log("there was an error");
-      }
-    );
+const addPoll = () => {
+  return {
+    type: ADD_POLL
   };
 };
