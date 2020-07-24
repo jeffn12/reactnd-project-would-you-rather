4377f8edcfb4634f5e2bc2ef34661bd932148cf6
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-24 13:54:09 -0400

    style: code spacing

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -16,6 +16,10 @@ import {
 import { handleAnswerPoll } from "../actions/shared";
 
 export class Poll extends Component {
+  /**
+   * Handle poll submission by dispatching the handleAnswerPoll event with the poll id, current user, and the selected option
+   * @param {String} option
+   */
   handleChange = (option) => {
     const { dispatch, authedUser, id } = this.props;
     dispatch(handleAnswerPoll(id, option, authedUser));
@@ -24,6 +28,10 @@ export class Poll extends Component {
   render() {
     const { polls, users, id, authedUser } = this.props;
 
+    // Ask user to login before showing the poll if there no authed user is set
+    if (!authedUser) return <LoginPage />;
+
+    // If the poll does not exist, show a "404: Not Found" error
     if (!polls[id])
       return (
         <Card>
@@ -31,44 +39,38 @@ export class Poll extends Component {
         </Card>
       );
 
-    if (!authedUser) return <LoginPage />;
-
-    const poll = polls[id];
-    const author = users[poll.author];
-    const currentUser = users[authedUser];
+    const poll = polls[id]; // Use the ID to get the poll Object
+    const author = users[poll.author]; // The user object of the person who created the poll
+    const currentUser = users[authedUser]; // The user object of the person who is answering the poll
 
     return (
-      <Card style={{ margin: "10px" }}>
+      <Card style={{ margin: "0.5rem" }}>
         <CardHeader
           avatar={<Avatar src={author.avatarURL} />}
           title={`${author.name} wants to know:`}
           subheader="would you rather..."
         />
-
         {poll[currentUser.answers[id]] ? (
           <AnsweredPollStats id={id} /> // If the user has already answered the poll, render the stats for it
         ) : (
-          <>
-            <CardContent>
-              <Button
-                fullWidth={true}
-                variant="outlined"
-                color="primary"
-                onClick={() => this.handleChange("optionOne")}
-              >
-                {poll.optionOne.text}
-              </Button>
-              <Typography align="center">or...</Typography>
-              <Button
-                fullWidth={true}
-                variant="outlined"
-                color="primary"
-                onClick={() => this.handleChange("optionTwo")}
-              >
-                {poll.optionTwo.text}
-              </Button>
-            </CardContent>
-          </>
+          // If the user has not answered the poll, render both options
+          <CardContent>
+            <Button
+              fullWidth={true}
+              variant="outlined"
+              color="primary"
+              onClick={() => this.handleChange("optionOne")}
+              children={poll.optionOne.text}
+            />
+            <Typography align="center">or...</Typography>
+            <Button
+              fullWidth={true}
+              variant="outlined"
+              color="primary"
+              onClick={() => this.handleChange("optionTwo")}
+              children={poll.optionTwo.text}
+            />
+          </CardContent>
         )}
       </Card>
     );
