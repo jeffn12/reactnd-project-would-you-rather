4f5f69af8931fe8965fc9cb20496f8d298b21ba1
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-18 10:35:18 -0400

    feat: add fetch call to add user to mongodb

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,5 @@
 import { _saveUser } from "../utils/_DATA";
+import { USERS_API_URI } from "../utils/vars";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 import { setAuthedUser } from "./authedUser";
 
@@ -13,6 +14,31 @@ export const handleAddUser = (user) => {
   return (dispatch) => {
     dispatch(showLoading());
     _saveUser(user)
+      .then((formattedUser) => {
+        dispatch(addUser(formattedUser));
+        dispatch(setAuthedUser(formattedUser.id));
+      })
+      .catch((err) => {
+        console.log("Error adding user: ", JSON.stringify(err));
+        alert(
+          `There was an error adding the user.  Please try again\n(error: status ${err.code}, ${err.message})`
+        );
+      });
+    dispatch(hideLoading());
+  };
+};
+
+// Send new user details to server for creation.  Server responds with a JSON user object
+export const _handleAddUser = (user) => {
+  return (dispatch) => {
+    dispatch(showLoading());
+    fetch(USERS_API_URI, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(user)
+    })
+      .then((response) => response.json())
       .then((formattedUser) => {
         dispatch(addUser(formattedUser));
         dispatch(setAuthedUser(formattedUser.id));
---
+++ src/utils/vars.js
@@ -0,0 +1,2 @@
+export const USERS_API_URI = "https://localhost:4242/api/users";
+export const QUESTIONS_API_URI = "https://localhost:4242/api/questions";
