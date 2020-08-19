fbd7b43c57922e6c35b43cad9b73677ec2ae4cf2
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-19 17:26:15 -0400

    feat: update api url

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,2 +1,4 @@
-export const USERS_API_URI = "http://localhost:4242/api/users";
-export const QUESTIONS_API_URI = "http://localhost:4242/api/questions";
+export const USERS_API_URI =
+  "https://sleepy-waters-91549.herokuapp.com/api/users";
+export const QUESTIONS_API_URI =
+  "https://sleepy-waters-91549.herokuapp.com/api/questions";
